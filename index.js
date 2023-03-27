38ec28d14abfa34a682c3654b2c54f91a2883f85
soo@utem.edu.my <soo@utem.edu.my>
2023-03-27 15:05:49 +0800

    exercise 1

---
+++ index.js
@@ -0,0 +1,44 @@
+// Exercise String and Number
+let data1 = true
+let data2 = "1.25"
+// console.log(data1, typeof data1)
+// console.log(data2, typeof data2)
+
+let data3 = data1.toString() + data2
+// console.log(data3, typeof data3)
+
+
+// Array
+let data = ["one", 2, true]
+// console.log( data[2], typeof data[2] )
+
+// object
+let user = {
+    name: "soo",
+    faculty: "fkekk",
+    phone: "123456",
+    email: "soo@utem.edu.my"
+}
+// console.log( user.faculty )
+user = {
+    name: "soo",
+    faculty: "fkekk",
+    phone: {
+        office: "123456",
+        mobile: "654131"
+    },
+    email: "soo@utem.edu.my"
+}
+console.log( user.phone.mobile )
+console.log( user.phone.home )
+
+
+
+
+
+
+
+
+
+
+
