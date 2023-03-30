c6715c35b5c1e4370fb2365d480963dbe1aa8572
soo@utem.edu.my <soo@utem.edu.my>
2023-03-30 15:37:51 +0800

    101 javascript

---
+++ quiz1.js
@@ -0,0 +1,53 @@
+let message = "benr"
+// console.log(message, typeof message)
+
+message = 123
+// console.log(message, typeof message)
+
+
+
+let str = "1.258"
+let num = 1
+
+let total = parseFloat(str) + num
+// console.log(total, typeof total)
+
+let data = ["benr", 3523, true]
+// console.log(data[0])
+// console.log(data[1])
+// console.log(data[2])
+// console.log(data[3])
+
+let user = {
+    name: "soo",
+    age: 50,
+    faculty: "FKEKK"
+}
+// console.log(user.name)
+// console.log(user.age)
+
+let messages = [
+    {
+        name: "usr1",
+        date: "1/4/2023",
+        msg: "good video",
+        like: 1000
+    },
+    {
+        name: "usr2",
+        date: "2/4/2023",
+        msg: "very good video",
+        like: 3000
+    },
+]
+console.log(  )  // >> very good video
+
+
+
+
+
+
+
+
+
+
